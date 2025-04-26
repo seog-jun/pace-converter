2e7a3da92ba2a31a268478e2f2ed0d220818e3d9
James Hong <shi7117@naver.com>
2025-04-26 19:51:59 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+       <div>
+      <h1>Pace Converter 🏃‍♂️</h1>
+      <p>Let's calculate your running pace!</p>
+    </div>
+  );
+}
+
+export default App;
